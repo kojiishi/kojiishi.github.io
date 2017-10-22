f37b551ae3293a8b92634fee3bc2dcbece851f8d
Koji Ishii <kojii@chromium.org>
2017-10-23 01:39:55 +0900

    CSS Paint test: curly-bracket

--- css-paint/curly-bracket.js
+++ css-paint/curly-bracket.js
@@ -1,16 +1,15 @@
-﻿registerPaint('curly-bracket', class {
+registerPaint('curly-bracket', class {
   paint(ctx, geom, properties) {
-    const x = geom.width / 2;
-    const y = geom.height / 2;
     const radius = 5;
-    ctx.beginPath();
+    const height = geom.height;
+    const centerY = height / 2;
     ctx.moveTo(radius * 2, 0);
-    ctx.lineTo(radius, radius);
-    ctx.lineTo(radius, geom.height / 2 -radius);
-    ctx.lineTo(0, geom.height / 2);
-    ctx.lineTo(radius, geom.height / 2 + radius);
+    ctx.arcTo(radius, 0, radius, radius, radius);
+    ctx.lineTo(radius, centerY -radius);
+    ctx.arcTo(radius, centerY, 0, centerY, radius);
+    ctx.arcTo(radius, centerY, radius, centerY + radius, radius);
     ctx.lineTo(radius, geom.height - radius);
-    ctx.lineTo(radius * 2, geom.height);
+    ctx.arcTo(radius, height, radius * 2, height, radius);
     ctx.stroke();
   }
 });
